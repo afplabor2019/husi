1d6c0bac6ca02843f00a8f439b61bd48ef07bab8
Geng Julián <geng.julian@gmail.com>
2019-11-15 13:45:31 +0100

    Created MessageType.enum,Status.enum,User.class inside the messages package.

---
+++ Chat project/server/src/main/java/com/messages/MessageType.java
@@ -0,0 +1,5 @@
+package com.messages;
+
+public enum MessageType {
+    DISCONNECTED, CONNECTED, STATUS, USER, SERVER, NOTIFICATION, VOICE
+}
---
+++ Chat project/server/src/main/java/com/messages/Status.java
@@ -0,0 +1,5 @@
+package com.messages;
+
+public enum Status {
+    ONLINE, AWAY, BUSY
+}
