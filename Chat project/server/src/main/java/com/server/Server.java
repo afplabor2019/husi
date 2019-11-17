7e66d6dc082251fe8e8aa9f255297acd3b26fe87
Geng Julián <geng.julian@gmail.com>
2019-11-17 21:21:48 +0100

    Creating Handler class inside the server module.

--- Chat project/server/src/main/java/com/server/Server.java
+++ Chat project/server/src/main/java/com/server/Server.java
@@ -8,7 +8,9 @@ import com.messages.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -19,4 +21,38 @@ public class Server {
     private static HashSet<ObjectOutputStream> writers = new HashSet<>();
     private static ArrayList<User> users = new ArrayList<>();
     static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[]args) throws Exception{
+        logger.info("The chat server is running.");
+        ServerSocket listener = new ServerSocket(PORT);
+
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Handler extends Thread{
+        private String name;
+        private Socket socket;
+        private Logger logger = LoggerFactory.getLogger(Handler.class);
+        private User user;
+        private ObjectInputStream input;
+        private OutputStream os;
+        private ObjectOutputStream output;
+        private InputStream is;
+
+        public Handler(Socket socket) throws IOException {
+            this.socket = socket;
+        }
+    }
 }
+
+
+
+
