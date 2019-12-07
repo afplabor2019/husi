c546bec9e5a8d1f5b1bbf678b04c928e4bc95b9d
Geng Julián <geng.julian@gmail.com>
2019-12-07 13:12:13 +0100

    setUsernaemlabel,setImageLabel,setOnlineLabel is created.

--- Chat project/client/src/main/java/com/client/chatwindow/ChatController.java
+++ Chat project/client/src/main/java/com/client/chatwindow/ChatController.java
@@ -6,6 +6,7 @@ import com.messages.Message;
 import com.messages.MessageType;
 import com.messages.bubble.BubbleSpec;
 import com.messages.bubble.BubbledLabel;
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -58,15 +59,15 @@ public class ChatController implements Initializable {
                 profileImage.setFitHeight(32);
                 profileImage.setFitWidth(32);
                 BubbledLabel bl6 = new BubbledLabel();
-                if (msg.getType() == MessageType.VOICE){
+                if (msg.getType() == MessageType.VOICE) {
                     ImageView imageview = new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString()));
                     bl6.setGraphic(imageview);
                     bl6.setText("Sent a voice message!");
                     VoicePlayback.playAudio(msg.getVoiceMsg());
-                }else {
+                } else {
                     bl6.setText(msg.getName() + ": " + msg.getMsg());
                 }
-                bl6.setBackground(new Background(new BackgroundFill(Color.WHITE,null, null)));
+                bl6.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
                 HBox x = new HBox();
                 bl6.setBubbleSpec(BubbleSpec.FACE_LEFT_CENTER);
                 x.getChildren().addAll(profileImage, bl6);
@@ -75,4 +76,17 @@ public class ChatController implements Initializable {
                 return x;
             }
         };
+    }
+
+    public void setUsernameLabel(String username) {
+        this.usernameLabel.setText(username);
+    }
+
+    public void setImageLabel() throws IOException {
+        this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
+    }
+
+    public void setOnlineLabel(String usercount) {
+        Platform.runLater(() -> onlineCountLabel.setText(usercount));
+    }
 }
