852d08854d359e94e0348b882a3cd189b49b922b
Moss4t <55391909+Moss4t@users.noreply.github.com>
2020-01-23 20:55:44 +0100

    Create MessageSender.java

---
+++ Chat project/client/src/main/java/com/client/chatwindow/MessageSender.java
@@ -0,0 +1,280 @@
+package com.client.chatwindow;
+
+
+
+import com.client.login.MainLauncher;
+import com.client.util.VoicePlayback;
+import com.client.util.VoiceRecorder;
+import com.client.util.VoiceUtil;
+import com.messages.Message;
+import com.messages.MessageType;
+import com.messages.Status;
+import com.messages.User;
+import com.messages.bubble.BubbleSpec;
+import com.messages.bubble.BubbledLabel;
+import com.traynotifications.animations.AnimationType;
+import com.traynotifications.notification.TrayNotification;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatController implements Initializable {
+
+    @FXML private TextArea messageBox;
+    @FXML private Label usernameLabel;
+    @FXML private Label onlineCountLabel;
+    @FXML private ListView userList;
+    @FXML private ImageView userImageView;
+    @FXML private Button recordBtn;
+    @FXML ListView chatPane;
+    @FXML ListView statusList;
+    @FXML BorderPane borderPane;
+    @FXML ComboBox statusComboBox;
+    @FXML ImageView microphoneImageView;
+
+    Image microphoneActiveImage = new Image(getClass().getClassLoader().getResource("images/microphone-active.png").toString());
+    Image microphoneInactiveImage = new Image(getClass().getClassLoader().getResource("images/microphone.png").toString());
+
+    private double xOffset;
+    private double yOffset;
+    //Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+
+    public synchronized void addToChat(Message msg) {
+        Task<HBox> othersMessages = new Task<HBox>() {
+            @Override
+            public HBox call() throws Exception {
+                Image image = new Image(getClass().getClassLoader().getResource("images/" + msg.getPicture().toLowerCase() + ".png").toString());
+                ImageView profileImage = new ImageView(image);
+                profileImage.setFitHeight(32);
+                profileImage.setFitWidth(32);
+                BubbledLabel bl6 = new BubbledLabel();
+                if (msg.getType() == MessageType.VOICE) {
+                    ImageView imageview = new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString()));
+                    bl6.setGraphic(imageview);
+                    bl6.setText("Sent a voice message!");
+                    VoicePlayback.playAudio(msg.getVoiceMsg());
+                } else {
+                    bl6.setText(msg.getName() + ": " + msg.getMsg());
+                }
+                bl6.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+                HBox x = new HBox();
+                bl6.setBubbleSpec(BubbleSpec.FACE_LEFT_CENTER);
+                x.getChildren().addAll(profileImage, bl6);
+                //logger.debug("ONLINE USERS: " + Integer.toString(msg.getUserlist().size()));
+                setOnlineLabel(Integer.toString(msg.getOnlineCount()));
+                return x;
+            }
+        };
+    }
+
+    public void setImageLabel() throws IOException {
+        this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
+    }
+
+    public void setUserList(Message msg) {
+        // logger.info("setUserList() method Enter");
+        Platform.runLater(() -> {
+            ObservableList<User> users = FXCollections.observableList(msg.getUsers());
+            userList.setItems(users);
+            userList.setCellFactory(new CellRenderer());
+            setOnlineLabel(String.valueOf(msg.getUserlist().size()));
+        });
+        //logger.info("setUserList() method Exit");
+    }
+
+    public void setUsernameLabel(String username) {
+        this.usernameLabel.setText(username);
+    }
+
+    public void setOnlineLabel(String usercount) {
+        Platform.runLater(() -> onlineCountLabel.setText(usercount));
+    }
+
+    public void recordVoiceMessage() throws IOException {
+        if (VoiceUtil.isRecording()) {
+            Platform.runLater(() -> {
+                        microphoneImageView.setImage(microphoneInactiveImage);
+                    }
+            );
+            VoiceUtil.setRecording(false);
+        } else {
+            Platform.runLater(() -> {
+                        microphoneImageView.setImage(microphoneActiveImage);
+
+                    }
+            );
+            VoiceRecorder.captureAudio();
+        }
+    }
+
+    public void sendButtonAction() throws IOException {
+        String msg = messageBox.getText();
+        if (!messageBox.getText().isEmpty()) {
+            Listener.send(msg);
+            messageBox.clear();
+        }
+    }
+
+    public synchronized void addAsServer(Message msg) {
+        Task<HBox> task = new Task<HBox>() {
+            @Override
+            public HBox call() throws Exception {
+                BubbledLabel bl6 = new BubbledLabel();
+                bl6.setText(msg.getMsg());
+                // bl6.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE,
+                //         null, null)));
+                HBox x = new HBox();
+                bl6.setBubbleSpec(BubbleSpec.FACE_BOTTOM);
+                x.setAlignment(Pos.CENTER);
+                x.getChildren().addAll(bl6);
+                return x;
+            }
+        };
+        task.setOnSucceeded(event -> {
+            chatPane.getItems().add(task.getValue());
+        });
+
+        Thread t = new Thread(task);
+        t.setDaemon(true);
+        t.start();
+    }
+
+    public void sendMethod(KeyEvent event) throws IOException {
+        if (event.getCode() == KeyCode.ENTER) {
+            sendButtonAction();
+        }
+    }
+
+    @FXML
+    public void closeApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void logoutScene() {
+        Platform.runLater(() -> {
+            FXMLLoader fmxlLoader = new FXMLLoader(getClass().getResource("/views/LoginView.fxml"));
+            Parent window = null;
+            try {
+                window = (Pane) fmxlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Stage stage = MainLauncher.getPrimaryStage();
+            Scene scene = new Scene(window);
+            stage.setMaxWidth(350);
+            stage.setMaxHeight(420);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        });
+    }
+
+    public void newUserNotification(Message msg) {
+        Platform.runLater(() -> {
+            Image profileImg = new Image(getClass().getClassLoader().getResource("images/" + msg.getPicture().toLowerCase() +".png").toString(),50,50,false,false);
+            TrayNotification tray = new TrayNotification();
+            tray.setTitle("A new user has joined!");
+            tray.setMessage(msg.getName() + " has joined the JavaFX Chatroom!");
+            tray.setRectangleFill(Paint.valueOf("#2C3E50"));
+            tray.setAnimationType(AnimationType.POPUP);
+            tray.setImage(profileImg);
+            tray.showAndDismiss(Duration.seconds(5));
+            try {
+                Media hit = new Media(getClass().getClassLoader().getResource("sounds/notification.wav").toString());
+                MediaPlayer mediaPlayer = new MediaPlayer(hit);
+                mediaPlayer.play();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            setImageLabel();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setOnMousePressed(event -> {
+            xOffset = MainLauncher.getPrimaryStage().getX() - event.getScreenX();
+            yOffset = MainLauncher.getPrimaryStage().getY() - event.getScreenY();
+            borderPane.setCursor(Cursor.CLOSED_HAND);
+        });
+
+        borderPane.setOnMouseDragged(event -> {
+            MainLauncher.getPrimaryStage().setX(event.getScreenX() + xOffset);
+            MainLauncher.getPrimaryStage().setY(event.getScreenY() + yOffset);
+
+        });
+
+        borderPane.setOnMouseReleased(event -> {
+            borderPane.setCursor(Cursor.DEFAULT);
+        });
+
+        statusComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                try {
+                    Listener.sendStatusUpdate(Status.valueOf(newValue.toUpperCase()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        messageBox.addEventFilter(KeyEvent.KEY_PRESSED, ke -> {
+            if (ke.getCode().equals(KeyCode.ENTER)) {
+                try {
+                    sendButtonAction();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ke.consume();
+            }
+        });
+
+    }
+
+    public void setImageLabel(String selectedPicture) {
+        switch (selectedPicture) {
+            case "Dominic":
+                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
+                break;
+            case "Sarah":
+                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/sarah.png").toString()));
+                break;
+            case "Default":
+                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/default.png").toString()));
+                break;
+        }
+    }
+}
